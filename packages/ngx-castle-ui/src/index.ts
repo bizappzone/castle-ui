4e44e14d7a9b08c662c8aefc99c489930e851737
aj <art@ampsire.com>
2023-04-28 13:54:10 -0400

    add dependancies

--- packages/ngx-castle-ui/src/index.ts
+++ packages/ngx-castle-ui/src/index.ts
@@ -1,3 +1,5 @@
 export * from './lib/ngx-castle-ui.module';
 
 export * from './lib/button/button.component';
+export * from './lib/icon/icon.component';
+export * from './lib/badge/badge.component';
